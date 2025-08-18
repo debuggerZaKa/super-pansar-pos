dff0081a0f0a999ea533a46559b77b24e25e41c3
debuggerZaKa <2023CS596@student.uet.edu.pk>
2025-08-19 03:27:13 +0500

    Delete login.sql

--- login.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Username] NVARCHAR(50) NOT NULL, 
-    [Password] NVARCHAR(50) NOT NULL, 
-    CONSTRAINT [PK_Table] PRIMARY KEY ([Id]), 
-    CONSTRAINT [PK_Table] PRIMARY KEY ([Id])
-)
